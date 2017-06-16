2f282484264c2af47f896fa3c380426010faf190
Gavin Harcourt <gharcourt@brainboxdigital.com>
2017-06-16 12:30:54 +0100

    Update alb security group

--- security_group.tf
+++ security_group.tf
@@ -8,4 +8,11 @@ resource "aws_security_group" "web" {
     protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+   }
 }
