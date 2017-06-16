d7e65aedc0329470b483655d943699b6400d5f16
Gavin Harcourt <gharcourt@brainboxdigital.com>
2017-06-16 10:53:50 +0100

    skip final rds cluster snapshot to allow destroy

--- rds_cluster.tf
+++ rds_cluster.tf
@@ -6,8 +6,10 @@ resource "aws_rds_cluster_instance" "hawordpress_rds_cluster_instances" {
 }
 
 resource "aws_rds_cluster" "hawordpress_rds_cluster" {
-  cluster_identifier = "aurora-cluster-hawordpress"
-  availability_zones = ["${var.region}a", "${var.region}b", "${var.region}c"]
-  master_username    = "${var.rdsuser}"
-  master_password    = "${var.rdspassword}"
+  cluster_identifier  = "aurora-cluster-hawordpress"
+  availability_zones  = ["${var.region}a", "${var.region}b", "${var.region}c"]
+  master_username     = "${var.rdsuser}"
+  master_password     = "${var.rdspassword}"
+  final_snapshot_identifier = "some-snap"
+  skip_final_snapshot = "True"
 }
